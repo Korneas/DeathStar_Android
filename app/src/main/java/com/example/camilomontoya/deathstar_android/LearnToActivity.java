5bcf855a41bc1c6d9f20377e432c419beca86693
CamiloMontoya <cajomo0311@gmail.com>
2017-04-03 01:08:54 -0500

    Communication with Java_Eclipse for items

--- app/src/main/java/com/example/camilomontoya/deathstar_android/LearnToActivity.java
+++ app/src/main/java/com/example/camilomontoya/deathstar_android/LearnToActivity.java
@@ -1,57 +1,52 @@
 package com.example.camilomontoya.deathstar_android;
 
 import android.content.Intent;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-import java.util.Observable;
-import java.util.Observer;
-
 import serial.MessageSerial;
 
-public class MainActivity extends AppCompatActivity implements Observer{
+public class LearnToActivity extends AppCompatActivity {
 
-    private ImageButton start, instruc;
+    private ImageButton back, next;
     private static String GROUP_ADDRESS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_learn_to);
 
-        start = (ImageButton) findViewById(R.id.start);
-        instruc = (ImageButton) findViewById(R.id.instruc);
+        back = (ImageButton) findViewById(R.id.backing);
+        next = (ImageButton) findViewById(R.id.next);
 
         GROUP_ADDRESS = Comunicacion.getInstance().getGROUP_ADDRESS();
 
-        start.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Comunicacion.getInstance().enviar(new MessageSerial(10,0, "online"), GROUP_ADDRESS);
-                startActivity(new Intent(MainActivity.this,PlayActivity.class));
+                startActivity(new Intent(LearnToActivity.this,MainActivity.class));
+                overridePendingTransition(R.anim.left_in, R.anim.right_out);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aviso("Gracias por ver el tutorial +10 prrro");
+                startActivity(new Intent(LearnToActivity.this,MainActivity.class));
                 overridePendingTransition(R.anim.right_in, R.anim.left_out);
             }
         });
 
-        instruc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,LearnToActivity.class));
-                overridePendingTransition(R.anim.right_in, R.anim.left_out);
-            }
-        });
+
     }
 
     public void aviso(String msg) {
         Toast notificacion = Toast.makeText(this, msg, Toast.LENGTH_LONG);
         notificacion.show();
-    }
-
-    @Override
-    public void update(Observable o, Object arg) {
-
     }
 }
--- app/src/main/java/com/example/camilomontoya/deathstar_android/PlayActivity.java
+++ app/src/main/java/com/example/camilomontoya/deathstar_android/PlayActivity.java
@@ -2,12 +2,159 @@ package com.example.camilomontoya.deathstar_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class PlayActivity extends AppCompatActivity {
+import java.util.Observable;
+import java.util.Observer;
+
+import serial.Item;
+import serial.Usuario;
+
+public class PlayActivity extends AppCompatActivity implements Observer{
+
+    private ToggleButton player1, player2, player3;
+    private ImageButton r2d2,fighter,bomber,stroom;
+    private String GROUP_ADDRESS;
+    private String j;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        Comunicacion.getInstance().addObserver(this);
+
+        GROUP_ADDRESS = Comunicacion.getInstance().getGROUP_ADDRESS();
+
+        player1 = (ToggleButton) findViewById(R.id.player1);
+        player2 = (ToggleButton) findViewById(R.id.player2);
+        player3 = (ToggleButton) findViewById(R.id.player3);
+
+        r2d2 = (ImageButton) findViewById(R.id.r2d2);
+        fighter = (ImageButton) findViewById(R.id.fighter);
+        bomber = (ImageButton) findViewById(R.id.bomber);
+        stroom = (ImageButton) findViewById(R.id.stroom);
+
+        r2d2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(player1.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(1,1),GROUP_ADDRESS);
+                }
+
+                if(player2.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(2,1),GROUP_ADDRESS);
+                }
+
+                if(player3.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(3,1),GROUP_ADDRESS);
+                }
+
+                if(!player1.isChecked() && !player2.isChecked() && !player3.isChecked()){
+                    aviso("Selecciona almenos un jugador");
+                }
+            }
+        });
+
+        fighter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(player1.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(1,2),GROUP_ADDRESS);
+                }
+
+                if(player2.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(2,2),GROUP_ADDRESS);
+                }
+
+                if(player3.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(3,2),GROUP_ADDRESS);
+                }
+
+                if(!player1.isChecked() && !player2.isChecked() && !player3.isChecked()){
+                    aviso("Selecciona almenos un jugador");
+                }
+            }
+        });
+
+        bomber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(player1.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(1,3),GROUP_ADDRESS);
+                }
+
+                if(player2.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(2,3),GROUP_ADDRESS);
+                }
+
+                if(player3.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(3,3),GROUP_ADDRESS);
+                }
+
+                if(!player1.isChecked() && !player2.isChecked() && !player3.isChecked()){
+                    aviso("Selecciona almenos un jugador");
+                }
+            }
+        });
+
+        stroom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(player1.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(1,4),GROUP_ADDRESS);
+                }
+
+                if(player2.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(2,4),GROUP_ADDRESS);
+                }
+
+                if(player3.isChecked()){
+                    Comunicacion.getInstance().enviar(new Item(3,4),GROUP_ADDRESS);
+                }
+
+                if(!player1.isChecked() && !player2.isChecked() && !player3.isChecked()){
+                    aviso("Selecciona almenos un jugador");
+                }
+            }
+        });
+    }
+
+    public void aviso(String msg) {
+        Toast notificacion = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        notificacion.show();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if(arg instanceof Usuario){
+            Usuario u = (Usuario) arg;
+            if(u.getEmisor()==1){
+                nuevoUsuario(player1,u.getName());
+            }
+
+            if(u.getEmisor()==2){
+                nuevoUsuario(player2,u.getName());
+            }
+
+            if(u.getEmisor()==3){
+                nuevoUsuario(player3,u.getName());
+            }
+        }
+    }
+
+    private void nuevoUsuario(final ToggleButton t,final String name){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                j = t.getText()+"\n"+name.substring(0,name.length()-1);
+                t.setText(j);
+                t.setTextOff(j);
+                t.setTextOn(j);
+            }
+        });
     }
 }
